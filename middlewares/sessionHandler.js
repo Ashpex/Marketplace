f07fef463f3211f6bacc14f937a40e6da903db50
ashpex <ashpex@pm.me>
2021-12-13 16:04:50 +0700

    add uuid

--- middlewares/sessionHandler.js
+++ middlewares/sessionHandler.js
@@ -1,6 +1,7 @@
+const {uuid} = require('uuidv4');
 module.exports = function(req, res, next){
     if (!req.session.unauthId){
-        req.session.unauthId = uuid.v4();
+        req.session.unauthId = uuid();
     }
     next();
 }
