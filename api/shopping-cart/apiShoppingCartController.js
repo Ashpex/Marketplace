16ca3e2bbd5a92f76f7401ca93d8f2ad0dda923e
peakyblinder244 <lhquan244@gmail.com>
2022-01-01 23:21:09 +0700

    add shopping cart with ajax (add one product)

--- api/shopping-cart/apiShoppingCartController.js
+++ api/shopping-cart/apiShoppingCartController.js
@@ -1,6 +1,7 @@
 const Session = require("../../models/Session");
 const ShoppingCart = require("../../models/ShoppingCart");
 const ProductOrder = require("../../models/ProductOrder");
+const User = require("../../models/User");
 
 module.exports = {
   addShoppingCart: async (req, res) => {
@@ -55,6 +56,58 @@ module.exports = {
       }
 
       console.log(session);
+    } else {
+      const session = await Session.findOne({ idUser: req.session.unauthId });
+      const shoppingCart = await ShoppingCart.findById(session.idShoppingCart);
+      await User.findOneAndUpdate(
+        { email: req.user.email },
+        {
+          idShoppingCart: shoppingCart._id,
+        }
+      );
+      let listProductOrder = [];
+      for await (let idProductOrder of shoppingCart.listProductOrder) {
+        let productOrder = await ProductOrder.findById(idProductOrder);
+        listProductOrder.push(productOrder);
+      }
+      let id = await containsProduct(idProduct, listProductOrder);
+      if (id === null) {
+        const productOrder = new ProductOrder({
+          idProduct: idProduct,
+          quantity: 1,
+        });
+        await productOrder.save((err, data) => {
+          if (err) {
+            console.log(err);
+            return res.status(500).json({ result: "failed", message: err });
+          } else {
+            console.log(data);
+            console.log(shoppingCart);
+            ShoppingCart.findByIdAndUpdate(
+              shoppingCart._id,
+              {
+                $push: {
+                  listProductOrder: data._id,
+                },
+              },
+              (err, hihi) => {
+                if (err) {
+                  console.log(err);
+                } else {
+                  console.log(hihi);
+                }
+              }
+            );
+          }
+        });
+      } else {
+        const productOrder = await ProductOrder.findById(id);
+        let count = productOrder.quantity;
+        await ProductOrder.findByIdAndUpdate(id, {
+          quantity: count + 1,
+        });
+      }
+      console.log(req.user);
     }
     return res.status(200).json({
       result: "ok",
