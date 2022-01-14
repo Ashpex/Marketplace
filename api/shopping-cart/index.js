76c0c9f95aff030e7f9dfc11d798ea0e448a25ba
ashpex <ashpex@pm.me>
2022-01-14 15:20:00 +0700

    Update API and index

--- api/shopping-cart/index.js
+++ api/shopping-cart/index.js
@@ -5,5 +5,14 @@ const apiShoppingCartController = require("./apiShoppingCartController");
 
 router.post("/addShoppingCart", apiShoppingCartController.addShoppingCart);
 router.get("/getShoppingCart", apiShoppingCartController.getShoppingCart);
+router.put(
+  "/removeProductOder",
+  apiShoppingCartController.putRemoveProductOder
+);
+
+router.put(
+  "/updateShoppingCart",
+  apiShoppingCartController.putUpdateShoppingCart
+);
 
 module.exports = router;
