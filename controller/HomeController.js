b65f9af732d3e7977aea5acafd3ccbdf34d1036e
peakyblinder244 <lhquan244@gmail.com>
2022-01-17 01:32:55 +0700

    update home ui

--- controller/HomeController.js
+++ controller/HomeController.js
@@ -6,9 +6,26 @@ const productController = require("./ProductController");
 
 module.exports = {
   index: async (req, res, next) => {
-    const categories = await Category.find({});
+    const categories = await Category.find({}).lean();
+
+    let categoryData = categories;
+    for (let i = 0; i < categoryData.length; i++) {
+      categoryData[i].listProduct = [];
+      for (let j = 0; j < categoryData[i].listIdProduct.length; j++) {
+        let product = await Product.findById(
+          categoryData[i].listIdProduct[j]
+        ).lean();
+
+        categoryData[i].listProduct.push(product);
+        if (j === 3) {
+          break;
+        }
+      }
+    }
+
     res.render("home/home", {
-      category: utils.mutipleMongooseToObject(categories),
+      category: categories,
+      categoryData: categoryData,
     });
   },
 };
