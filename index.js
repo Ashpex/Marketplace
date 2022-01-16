57fb8485104fd182559082a35dc7dbb86bc9855e
ashpex <ashpex@pm.me>
2022-01-16 19:02:16 +0700

    add

--- index.js
+++ index.js
@@ -126,58 +126,57 @@ app.use("/api/shoppingCart", apiShoppingCartRouter);
 app.use(userShoppingCart);
 route(app);
 
-express().use(bodyParser.json()).post('/webhook', (req, res) => {  
- 
-  let body = req.body;
+express()
+  .use(bodyParser.json())
+  .post("/webhook", (req, res) => {
+    // Parse the request body from the POST
+    let body = req.body;
 
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
+    // Check the webhook event is from a Page subscription
+    if (body.object === "page") {
+      // Iterate over each entry - there may be multiple if batched
+      body.entry.forEach(function (entry) {
+        // Gets the body of the webhook event
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
 
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
+        // Get the sender PSID
+        let sender_psid = webhook_event.sender.id;
+        console.log("Sender PSID: " + sender_psid);
+      });
 
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-    });
-
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-
-});
-
-express().use(bodyParser.json()).get('/webhook', (req, res) => {
-
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "fbverifytoken"
-    
-  // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-    
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-  
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-    
+      // Return a '200 OK' response to all events
+      res.status(200).send("EVENT_RECEIVED");
     } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
+      // Return a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
     }
-  }
-});
+  });
 
+express()
+  .use(bodyParser.json())
+  .get("/webhook", (req, res) => {
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = "fbverifytoken";
+
+    // Parse the query params
+    let mode = req.query["hub.mode"];
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
+
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+      // Checks the mode and token sent is correct
+      if (mode === "subscribe" && token === VERIFY_TOKEN) {
+        // Responds with the challenge token from the request
+        console.log("WEBHOOK_VERIFIED");
+        res.status(200).send(challenge);
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);
+      }
+    }
+  });
 
 app.use((req, res) => {
   res.render("errors/404", { layout: false });
